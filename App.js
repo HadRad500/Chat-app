d7503d586ea4b17e6c3b4beaaadb77cd4602cd40
HadRad500 <hadignasr@gmail.com>
2023-12-18 13:29:45 -0800

    cleaned up code

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
 import React, { useEffect } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-// import the screens
-import Start from './components/Start';
-import Chat from './components/Chat';
-// import react Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { initializeApp } from "firebase/app";
-import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
-import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import { LogBox, Alert } from "react-native";
-import { useNetInfo } from "@react-native-community/netinfo";
-import { getStorage } from "firebase/storage";
 import { DatabaseProvider } from "./components/DatabaseContext";
 import Wrapper from "./components/Wrapper";
-
-// Create the navigator
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
